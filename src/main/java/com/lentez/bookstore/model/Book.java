b0b3d271cb0b106d5ac04eb4cf8bc17023a24d35
Lentezz <yaroslav.golovan@gmail.com>
2026-01-31 00:30:18 +0200

    fix book class

--- src/main/java/com/lentez/bookstore/model/Book.java
+++ src/main/java/com/lentez/bookstore/model/Book.java
@@ -7,15 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "books")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
